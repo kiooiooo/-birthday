81955498ebf962e792f54927e3db02c54a36347c
kiooiooo <93810948+kiooiooo@users.noreply.github.com>
2021-11-06 12:26:32 +0800

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "心爱的小可爱": "C:\Users\burner\Desktop\微信图片_20211106121627.jpg",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
@@ -33,6 +33,6 @@ var config = {
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
-        story: "Follow your heart,Tao bangguo",
+        story: "Follow your heart,陶邦国",
     }
 };
